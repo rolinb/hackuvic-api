ad3ea2e2e296cc9c90307def21f5eaceee2b05b1
rolin <rolin_B@hotmail.com>
2017-03-11 21:40:13 -0800

    comma

--- app/models/cards.js
+++ app/models/cards.js
@@ -2,7 +2,7 @@ var mongoose     = require('mongoose');
 var Schema       = mongoose.Schema;
 
 var CardSchema   = new Schema({
-	user_id: String
+	user_id: String,
   card:{
     question: String,
     answer: String
