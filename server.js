593aa2f75323a8b392b7807aef177f145582be41
rolin <rolin_B@hotmail.com>
2017-03-12 06:02:43 -0700

    allow local?

--- server.js
+++ server.js
@@ -31,6 +31,7 @@ var router = express.Router();
 // middleware to use for all requests
 router.use(function(req, res, next) {
 	// do logging
+	    res.setHeader('Access-Control-Allow-Origin', 'http://localhost:80');
 	console.log('Something is happening.');
 	next();
 });
