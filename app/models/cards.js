6747550d618cb1734363e82bee2ef5ee53a0968c
rolin <rolin_B@hotmail.com>
2017-03-11 21:39:07 -0800

    adding user_id?
    testing

--- app/models/cards.js
+++ app/models/cards.js
@@ -2,7 +2,11 @@ var mongoose     = require('mongoose');
 var Schema       = mongoose.Schema;
 
 var CardSchema   = new Schema({
-	name: String
+	user_id: String
+  card:{
+    question: String,
+    answer: String
+  }
 });
 
 module.exports = mongoose.model('Card', CardSchema);
